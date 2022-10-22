543bf100f32b0cd78f7ab4d306a1d4a94dc06fd1
Paul Landes <landes@mailc.net>
2022-10-22 12:42:17 -0500

    fix 0 arg get_instance for client apps

--- src/python/zensols/showfile/cli.py
+++ src/python/zensols/showfile/cli.py
@@ -3,7 +3,7 @@
 """
 __author__ = 'Paul Landes'
 
-from typing import List, Any, Dict
+from typing import List, Any, Dict, Union
 import sys
 from zensols.cli import ActionResult, CliHarness
 from zensols.cli import ApplicationFactory as CliApplicationFactory
@@ -13,6 +13,11 @@ class ApplicationFactory(CliApplicationFactory):
     def __init__(self, *args, **kwargs):
         kwargs['package_resource'] = 'zensols.showfile'
         super().__init__(*args, **kwargs)
+
+    def get_instance(self, args: Union[List[str], str] = None) -> Any:
+        if args is None:
+            args = 'config'
+        return super().get_instance(args)
 
 
 def main(args: List[str] = sys.argv, **kwargs: Dict[str, Any]) -> ActionResult:
