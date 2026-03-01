61734d0e96716b0d405e746df2c13a5ecd73a514
Paul Landes <landes@mailc.net>
2026-02-28 20:46:58 -0600

    release: doc; deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 ## [Unreleased]
 
 
+## [1.3.2] - 2026-02-28
 ### Added
 - File extension and type filtering so browsers don't fail on bad file types.
 - Support for rendering [zensols.datdesc] figures.
@@ -133,7 +134,8 @@ Downstream moderate risk update release.
 
 
 <!-- links -->
-[Unreleased]: https://github.com/plandes/rend/compare/v1.3.1...HEAD
+[Unreleased]: https://github.com/plandes/rend/compare/v1.3.2...HEAD
+[1.3.2]: https://github.com/plandes/rend/compare/v1.3.1...v1.3.2
 [1.3.1]: https://github.com/plandes/rend/compare/v1.3.0...v1.3.1
 [1.3.0]: https://github.com/plandes/rend/compare/v1.2.5...v1.3.0
 [1.2.5]: https://github.com/plandes/rend/compare/v1.2.4...v1.2.5
--- harness.py
+++ harness.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 
-import os
-from zensols.cli import ConfigurationImporterCliHarness
 
 if (__name__ == '__main__'):
+    import os
+    from zensols.cli import ConfigurationImporterCliHarness
     locs: str = {
         0: 'test-resources/sample.pdf',
         1: 'http://example.com',
         2: 'test-resources/sample.pdf,http://example.com',
         3: f'file://{os.getcwd()}/test-resources/sample.pdf',
         4: f'file://{os.getcwd()}/test-resources/sample.pdf -t url',
-        5: 'test-resources/states.csv',
-    }[5]
+        5: 'test-resources/cc.png',
+        6: 'test-resources/states.csv',
+        7: 'test-resources/renderables/roster-table.json',
+        8: 'test-resources/renderables/roster-figure.yml',
+    }[8]
     harness = ConfigurationImporterCliHarness(
         src_dir_name='src',
         app_factory_class='zensols.rend.ApplicationFactory',
         proto_args=f'show -H 1200 -W 1200 {locs}',
         proto_factory_kwargs={
-            'reload_pattern': r'^zensols\.rend\.?(?!domain)'},
+            'reload_pattern': r'^zensols\.rend\.?(?!(?:domain|df))\w+'},
     )
     harness.run()
